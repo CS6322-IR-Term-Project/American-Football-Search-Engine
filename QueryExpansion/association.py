94032faae928f7351cddfb7d945c4dc9c31336c2
Gaurav Joshi <gaurav.joshi@utdallas.edu>
2023-04-19 01:50:45 -0500

    V1 Prototype

---
+++ QueryExpansion/association.py
@@ -0,0 +1,196 @@
+import re
+
+import numpy as np
+from nltk.corpus import stopwords
+from nltk import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import pysolr
+import json
+from tqdm import tqdm
+from collections import defaultdict, Counter
+import heapq
+
+porter_stemmer = PorterStemmer()
+stop_words = set(stopwords.words('english'))
+
+
+def tokenize_text(text):
+    # remove enters
+    text = text.replace('\n', ' ')
+    # convert to lowercase
+    text = text.lower()
+    # tokenize text
+    tokens = word_tokenize(text)
+    # remove punctuation and numbers
+    tokens = [token for token in tokens if token.isalpha()]
+    # remove stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token not in stop_words]
+    
+    return tokens
+
+def make_stem_map(vocab):
+    """
+    Args:
+        vocab(list): a list of vocabulary
+
+    Returns:
+        token_2_stem(dict): a map from token to its stem having structure {token:stem}
+        stem_2_tokens(dict): a map from stem to its corresponding tokens having structure:
+                             {stem:set(token_1, token_2, ...)}
+    """
+    token_2_stem = {} # 1 to 1
+    stem_2_tokens = {} # 1 to n
+
+    for token in vocab:
+        stem = porter_stemmer.stem(token)
+        if stem not in stem_2_tokens:
+            stem_2_tokens[stem] = set()
+        stem_2_tokens[stem].add(token)
+        token_2_stem[token] = stem
+
+    return token_2_stem, stem_2_tokens 
+
+
+def build_association(doc_tokens, token_2_stem, stem_2_tokens, query, num_expansions=10):
+    """
+    Args:
+        doc_tokens(2-D list): tokens in each documents having structure:
+                              [[token_1, token_2, ...], [...], ...]
+        token_2_stem(dict): a map from token to its stem having structure {token:stem}
+        stem_2_tokens(dict): a map from stem to its corresponding tokens having structure:
+                             {stem:set(token_1, token_2, ...)}
+        query(list): a list of tokens from query
+        num_expansions(int): the number of expansion tokens to return for each query token
+        
+    Return:
+        query_expands(list): list of expand stem tokens ids for each token in the query
+    """
+    # build map from stem to index
+    stems = sorted(stem_2_tokens.keys())
+    stem_2_idx = {s: i for i, s in enumerate(stems)}
+
+    # frequency of stems in each document
+    f = np.zeros((len(doc_tokens), len(stems)), dtype=np.int64)
+    for doc_id, tokens in enumerate(doc_tokens):
+        stems_in_doc = set(token_2_stem.get(t, t) for t in tokens)
+        stem_counts = Counter(stems_in_doc)
+        for stem, count in stem_counts.items():
+            f[doc_id, stem_2_idx[stem]] = count
+
+    # correlation matrix
+    c = np.dot(f.T, f)
+
+    # normalize correlation matrix and pick the top expansion tokens
+    query_expands_id = []
+    for token in query:
+        stem = token_2_stem.get(token, token)
+        stem_id = stem_2_idx.get(stem)
+        if stem_id is None:
+            continue
+
+        # normalize correlation matrix for the query token
+        c_token = c[stem_id, :]
+        norm = np.linalg.norm(c_token)
+        if norm == 0:
+            continue
+        s_token = c_token / norm
+
+        # pick the top expansion stems for each query token
+        idx_sort = heapq.nlargest(num_expansions + 1, range(len(s_token)), key=s_token.__getitem__)
+        if stem_id in idx_sort:
+            idx_sort.remove(stem_id)  # remove the original stem from the list of expansions
+        else:
+            idx_sort.pop()  # remove the last expansion stem to keep only num_expansions stems
+        query_expands_id.extend(idx_sort)
+
+    # convert stem ids to stem
+    query_expands = [stems[stem_idx] for stem_idx in query_expands_id]
+
+    return query_expands
+                          
+
+
+
+def association_main(query, solr_results):
+    """
+    Args:
+        query(str): a text string of query
+        solr_results(list): result for the query from function 'get_results_from_solr'
+
+    Return:
+        query(str): a text string of expanded query
+    """
+    # query = 'olympic medal'
+    # solr = pysolr.Solr('http://localhost:8983/solr/nutch/', always_commit=True, timeout=10)
+    # results = get_results_from_solr(query, solr)
+    vocab = set()
+    doc_tokens = []
+
+    # tokenize query and query results, then build vocabulary
+    query_text = query # keep original query text
+
+    print("query text ==", query_text)
+
+    if 'content:' == query[:8]:
+        query = query[8:]
+    print("Initial Query ", query)
+    query = tokenize_text(query)
+    vocab.update(query)
+
+    for result in tqdm(solr_results, desc='Preprocessing results'):
+        if 'content' not in result:
+            tokens = set()
+        else:
+            tokens = set()
+
+            for token in result['content']:
+                tokens.update(tokenize_text(token))
+
+        doc_tokens.append(tokens)
+        vocab.update(tokens)
+
+        print(vocab)
+
+    vocab = list(sorted(vocab))
+    token_2_stem, stem_2_tokens = make_stem_map(vocab)
+
+    # expand query
+    query_expands_stem = build_association(doc_tokens, token_2_stem, stem_2_tokens, query)
+    
+    # convert from stem to tokens
+    query_expands = set()
+    for stem in query_expands_stem:
+        query_expands.update(list(stem_2_tokens[stem]))
+
+    # generate new query
+    for token in query:
+        query_expands.discard(token)
+
+    # update query with query expands 
+    expanded_query = list()
+    calc_query_list = list(query_expands)
+    query_index = 0
+    query_expands_index = 0
+
+    while query_expands_index < len(calc_query_list) and query_index < len(query):
+        expanded_query.append(query[query_index])
+        expanded_query.append(calc_query_list[query_expands_index])
+        query_index += 1
+        query_expands_index += 1
+    
+    while query_expands_index < len(calc_query_list):
+        expanded_query.append(calc_query_list[query_expands_index])
+        query_expands_index += 1
+
+    while query_index < len(query):
+        expanded_query.append(query[query_index])
+        query_index += 1
+
+    query = expanded_query
+
+    query = ' '.join(query)
+    print('Expanded query:', query)
+
+    return query
--- search_backend.py
+++ search_backend.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, url_for, request
 from googleapiclient.discovery import build
 from bs4 import BeautifulSoup
+from flask_cors import CORS
+from QueryExpansion.association import association_main
+from QueryExpansion.metric import metric_cluster_main
 import os, requests, json, re, html
+from spellchecker import SpellChecker
+from operator import itemgetter
 import pysolr
 
 app = Flask(__name__)
-
+CORS(app)
 solr = pysolr.Solr('http://localhost:8983/solr/nutch', timeout=10)
 
 @app.route("/", methods = ['GET', 'POST'])
@@ -15,13 +20,61 @@ def home_page():
 
     if request.method == 'POST':
         query = request.form['query']
+        user_selection = request.form.get('selection')
 
         # results for custom search, google and bing
         custom_results = custom_search(query)
-        
+
+        if is_hits(user_selection):
+            custom_results = get_hits_results(custom_results)
+
+        elif is_clustering(user_selection):
+            custom_results = get_clustering_results(custom_results, user_selection)
+
+        elif is_query_expansion(user_selection):
+            custom_results = query_expansion(query, custom_results, user_selection)  
+                  
         return render_template('index.html', custom_results = custom_results, google_results=google_results, bing_results=bing_results, query=query)
     
     return render_template('index.html', google_results=google_results, bing_results=bing_results)
+
+def is_hits(user_selection):
+    return user_selection == 'hits'
+
+def is_query_expansion(user_selection):
+    return user_selection in ['association-expansion', 'metric-expansion', 'scalar-expansion']
+
+def is_clustering(user_selection):
+    return user_selection in ['flat-clustering', 'hierarchical-clustering']
+
+def apply_spell_check(query):
+    spell = SpellChecker()
+
+    # split the sentence into individual words
+    words = query.split()
+
+    # find those words that may be misspelled
+    misspelled = spell.unknown(words)
+
+    # create a dictionary of corrections
+    corrections = {word: spell.correction(word) for word in misspelled}
+
+    return ' '.join(corrections.get(word, word) for word in words)
+
+def query_expansion(query, custom_results, selection):
+    query = apply_spell_check(query)
+    
+    if selection == 'association-expansion':
+        expanded_query = association_main(query, custom_results)
+        custom_results = custom_search(expanded_query)
+    elif selection == 'metric-expansion':
+        expanded_query = metric_cluster_main(query, custom_results)
+        custom_results = custom_search(expanded_query)
+    elif selection == 'scalar-expansion':
+        pass
+    
+    return custom_results
+
 
 def create_doc(result):
     doc = {
@@ -29,6 +82,8 @@ def create_doc(result):
         'title': result.get('title'),
         'url': result.get('url'),
         'anchor': result.get('anchor'),
+        'meta_info': result.get('meta_info'),
+        'digest': result.get('digest'),
         'content': None
     }
 
@@ -43,16 +98,76 @@ def create_doc(result):
 def custom_search(query):
     results = query_search(query)
     custom_results = [create_doc(result) for result in results]
+
     return custom_results
 
 def query_search(query):
-    results = solr.search('*:*', **{
-        'fl': 'id, title, url, anchor, content',  # Select the fields to return
-        'rows': 10
+    results = solr.search('text:' + query, **{
+        'fl': 'id, title, url, anchor, content, meta_info, digest',  # Select the fields to return
+        'rows': 50
     })
 
     docs = [create_doc(result) for result in results.docs]
     return docs
+
+
+def get_hits_results(clust_inp):
+    authority_score_file = open("RelevanceModel/authority_score_1", 'r').read()
+    authority_score_dict = json.loads(authority_score_file)
+
+    clust_inp = sorted(clust_inp, key=lambda x: authority_score_dict.get(x['url'][0], 0.0), reverse=True)
+
+    return clust_inp
+
+
+def get_clustering_results(custom_results, user_selection):
+    if user_selection == 'flat-clustering':
+        f = open('clustering_f.txt')
+        lines = f.readlines()
+        f.close()
+    elif user_selection == 'hierarchical-clustering':
+        f = open('clustering_h8.txt')
+        lines = f.readlines()
+        f.close()
+
+    cluster_map = {}
+
+    for line in lines:
+        line_split = line.split(",")
+        if line_split[1] == "":
+            line_split[1] = "99"
+        cluster_map.update({line_split[0]: line_split[1]})
+
+    for curr_resp in custom_results:
+        url_collection = curr_resp["url"][0]
+        curr_cluster = cluster_map.get(url_collection, "99")
+        curr_resp.update({"cluster": curr_cluster})
+        curr_resp.update({"done": "False"})
+        
+
+    clust_resp = []
+    curr_rank = 1
+
+    for curr_resp in custom_results:
+        if curr_resp["done"] == "False":
+            curr_cluster = curr_resp["cluster"]
+            curr_resp.update({"done": "True"})
+            curr_resp.update({"rank": str(curr_rank)})
+            curr_rank += 1
+            clust_resp.append({"title": curr_resp["title"], "url": curr_resp["url"], "content": curr_resp["content"],
+                               "meta_info": curr_resp["meta_info"], "rank": curr_resp["rank"]})
+            for remaining_resp in custom_results:
+                if remaining_resp["done"] == "False":
+                    if remaining_resp["cluster"] == curr_cluster:
+                        remaining_resp.update({"done": "True"})
+                        remaining_resp.update({"rank": str(curr_rank)})
+                        curr_rank += 1
+                        clust_resp.append({"title": remaining_resp["title"], "url": remaining_resp["url"],
+                                           "content": remaining_resp["content"],
+                                           "meta_info": remaining_resp["meta_info"], "rank": remaining_resp["rank"]})
+    
+    return clust_resp
+
 
 
 if __name__ == '__main__':
